17cee320d95501a06919bf602bada5dbbfa1b0dc
Ronan <ronan.dupas@ynov.com>
2024-11-22 14:39:24 +0100

    ajout règles du jeu dans rules.py et ajustement du main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from grille.grid import generate_grid, display_grid
+from logique.rules import next_state
 
-def verif_nb():
+def verify_nb():
     while True:
         try:
             taille = int(input("Entrez la taille de la grille : "))
@@ -12,13 +13,20 @@ def verif_nb():
             print("Entrée invalide. Veuillez entrer un entier.")
 
 def main():
-
-    taille = verif_nb()
+    taille = verify_nb()
 
     grille = generate_grid(taille)
 
-    print("\nGrille générée :")
-    display_grid(grille)
+    while True:
+        print("\nGrille actuelle :")
+        display_grid(grille)
+        
+        grille = next_state(grille)
+
+        action = input("\nAppuyez sur Entrée pour passer au tour suivant, ou Q pour quitter : ").strip().upper()
+        if action == 'Q':
+            print("Fin du jeu. Au revoir !")
+            break
 
 if __name__ == "__main__":
     main()
