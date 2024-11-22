c772934feaab194f0c6fd0703b94ef74478687f6
Ronan <ronan.dupas@ynov.com>
2024-11-22 15:49:24 +0100

    problèmes avec le merge

--- logique/rules.py
+++ logique/rules.py
@@ -11,31 +11,7 @@ def count_neighbors(grid, i, j):
         if 0 <= ni < n and 0 <= nj < n:
             living_neighbors += grid[ni][nj]
     return living_neighbors
-def count_neighbors(grid, i, j):
-    neighbors = [
-        (-1, -1), (-1, 0), (-1, 1),
-        (0, -1),         (0, 1),
-        (1, -1), (1, 0), (1, 1)
-    ]
-    n = len(grid)
-    living_neighbors = 0
-    for di, dj in neighbors:
-        ni, nj = i + di, j + dj
-        if 0 <= ni < n and 0 <= nj < n:
-            living_neighbors += grid[ni][nj]
-    return living_neighbors
 
-def next_state(grid):
-    n = len(grid)
-    new_grid = [[0] * n for _ in range(n)]
-    for i in range(n):
-        for j in range(n):
-            living_neighbors = count_neighbors(grid, i, j)
-            if grid[i][j] == 1:
-                if living_neighbors == 2 or living_neighbors == 3:
-                    new_grid[i][j] = 1
-                else:
-                    new_grid[i][j] = 0
 def next_state(grid):
     n = len(grid)
     new_grid = [[0] * n for _ in range(n)]
