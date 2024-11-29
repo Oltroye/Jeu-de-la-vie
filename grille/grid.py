ed06a8649c4d9481ad6caa55e0bd356b94506278
Ronan <ronan.dupas@ynov.com>
2024-11-29 14:10:06 +0100

    fonction dans grid.py supprimé

--- grille/grid.py
+++ grille/grid.py
@@ -6,12 +6,3 @@ def generate_grid(size):
 def display_grid(grid):
     for row in grid:
         print(' '.join('⬛' if cell == 1 else '⬜' for cell in row))
-
-def cycle_detect(grid, history):
-
-    if grid in history:
-        cycle_start = history.index(grid)
-        cycle_lenght = len(history) - cycle_start
-        return True, cycle_lenght
-    else:
-        return False, 0
